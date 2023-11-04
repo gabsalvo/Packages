a512b9201c36d183c2c5e413d6e48b886e0c3705
gabsalvo <g.salvo4@studenti.unipi.it>
2023-11-04 19:46:02 +0100

    Corrected

--- pip-packages/pip_spaced/pip_spaced/cli.py
+++ pip-packages/pip_spaced/pip_spaced/cli.py
@@ -1,18 +1,16 @@
 """Console script for pip_spaced."""
 import argparse
 import sys
-
+from pip_spaced import add_numbers  # Make sure to import the function if it's defined in another module
 
 def main():
     """Console script for pip_spaced."""
     parser = argparse.ArgumentParser(description='Add two numbers together.')
-    parser.add_argument('numbers', nargs='2', type=int, help="Two numbers to add")
+    parser.add_argument('numbers', nargs=2, type=int, help="Two numbers to add")
     args = parser.parse_args()
 
     result = add_numbers(*args.numbers)
     print(f"The result is {result}")
 
-
 if __name__ == "__main__":
     main()
-
